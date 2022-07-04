43683587ae8bc4e7c4a36c9ff66dbf8c23fe4aa7
k-linux-22 <the.kali.linux.22@outlook.com>
2022-07-04 12:44:38 +0530

    upload

---
+++ Python/file_handling.py
@@ -0,0 +1,15 @@
+''' syntax :
+file_object = open(name, mode, buffering)
+where, 
+name: name of the file
+mode: mode in which the file is to be opened
+buffering: if value is (0), no buffering occurs, if value is (1), line buffering is performed
+'''
+# create a new file or write a file by using 'w'
+# f = open('/home/klinux/Documents/Xplore/Python/new.py', 'w')
+
+# read the contents of a python file line by line by using 'r'
+f = open('/home/klinux/Documents/Xplore/Python/new.py', 'r')
+for each in f:
+    print(each)
+
