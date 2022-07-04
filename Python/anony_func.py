611ef527815f084337a7bd2f6b0987f198b293c4
k-linux-22 <the.kali.linux.22@outlook.com>
2022-07-04 14:12:18 +0530

    upload

---
+++ Python/anony_func.py
@@ -0,0 +1,44 @@
+""" anonymous functions """
+
+''' Lambda function '''
+''' 
+- used to create small functions
+- a one-time use function 
+- can have many arguments, but only one expression
+- cannot have a return statement
+- returns a function object which can be assigned to a variable
+- syntax ->     lambda argument(s) : expression
+'''
+
+''' rather than using -
+def sqr_of_num(num):
+    return num**2
+we can use - 
+'''
+sqr_of_num = lambda x : x**2
+print(sqr_of_num(5))
+print(type(sqr_of_num))
+
+''' Map '''
+'''
+- used to perfrom an operation on all the input items
+- arguments are provided as function object and any number of iterables like list, dictionary, etc.
+- syntax ->    map(function object, iterable 1, iterable 2, ...)
+'''
+
+# to print output, always use a data type(list, tuple) because map does not show the output with print
+
+print( list( map( lambda x : x*x, [1, 2, 3, 4, 5] ) ) )
+
+''' for better visualization - 
+
+print( 
+    list( 
+        map( 
+            lambda x : x*x, [1, 2, 3, 4, 5] 
+            ) 
+        ) 
+    )
+
+'''
+
--- Python/file_handling.py
+++ Python/file_handling.py
@@ -1,5 +1,4 @@
-''' syntax :
-file_object = open(name, mode, buffering)
+''' syntax ->   file_object = open(name, mode, buffering)
 where, 
 name: name of the file
 mode: mode in which the file is to be opened
@@ -7,26 +6,27 @@ buffering: if value is (0), no buffering occurs, if value is (1), line buffering
 '''
 
 ''' create a new file or write a file by using 'w' '''
-# f = open('/home/klinux/Documents/Xplore/Python/new.py', 'w')
+f = open('/home/klinux/Documents/Xplore/Python/new.py', 'w')
 
 ''' read the contents of a python file line by line by using 'r' '''
-# f = open('/home/klinux/Documents/Xplore/Python/new.py', 'r')
-# for each in f:
-#     print(each)
+f = open('/home/klinux/Documents/Xplore/Python/new.py', 'r')
+for each in f:
+    print(each)
 
 # read() can also be used to extract data or read data from a file
-# print(f.read()) 
+print(f.read()) 
 
-# f = open('/home/klinux/Documents/Xplore/Python/new.py', 'w')
-# f.write(" # A new line ")
+f = open('/home/klinux/Documents/Xplore/Python/new.py', 'w')
+f.write(" # A new line ")
 ''' now, all previous data in the file is replaced by # A new file '''
-# f.close()
+f.close()
 ''' the close() command terminates all resources in use and frees the system of this particular program '''
 
+
 ''' 'a' is used to add new data to a file '''
-# f = open('/home/klinux/Documents/Xplore/Python/new.py', 'a')
-# f.write("\n# This will add a new line")
-# f.close()
+f = open('/home/klinux/Documents/Xplore/Python/new.py', 'a')
+f.write("\n# This will add a new line")
+f.close()
 
 ''' 'x' is used to create a new file. If a file already exists, the operation fails '''
 
@@ -35,7 +35,7 @@ buffering: if value is (0), no buffering occurs, if value is (1), line buffering
 ''' renaming and deleting a file '''
 import os
 ''' syntax : os.rename("current_file_name", "new_file_name") '''
-# os.rename("/home/klinux/Documents/Xplore/Python/new.py", "/home/klinux/Documents/Xplore/Python/new_named.py")
+os.rename("/home/klinux/Documents/Xplore/Python/new.py", "/home/klinux/Documents/Xplore/Python/new_named.py")
 
 ''' syntax : os.remove("file_name") '''
 os.remove("/home/klinux/Documents/Xplore/Python/new_named.py")
